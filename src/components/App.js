2204203e008732622297e922666930133abf1d6c
MayVargasF <maytevargasfelix@gmail.com>
2022-08-19 08:50:45 +0200

    react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/App.scss";
+import quotes from "../data/quotes.json";
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
